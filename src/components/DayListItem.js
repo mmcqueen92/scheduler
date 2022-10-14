cb7a5b7ceb9055e9a0b0f55ede96381d16e63d53
Malcolm McQueen <malcolmmcqueen@live.com>
2022-10-13 19:33:00 -0700

    fix dayListItem spots remaining text for different outcomes

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,10 +7,34 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   })
-  return (
-    <li className={classes} onClick={() => props.setDay(props.name)}>
+  if (props.spots === 0) {
+    return (
+      <li className={classes} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">no spots remaining</h3>
     </li>
-  );
+    )
+  } else if (props.spots === 1) {
+    return (
+      <li className={classes} onClick={() => props.setDay(props.name)}>
+        <h2 className="text--regular">{props.name}</h2> 
+        <h3 className="text--light">{props.spots} spot remaining</h3>
+      </li>
+    );
+
+  } else if (props.spots > 1) {
+    return (
+      <li className={classes} onClick={() => props.setDay(props.name)}>
+        <h2 className="text--regular">{props.name}</h2> 
+        <h3 className="text--light">{props.spots} spots remaining</h3>
+      </li>
+    );
+  } else if (!props.spots) {
+    return (
+      <li className={classes} onClick={() => props.setDay(props.name)}>
+        <h2 className="text--regular">{props.name}</h2> 
+        <h3 className="text--light"></h3>
+      </li>
+    );
+  }
 }
